23a6d8e90a841299a81ca2e0fde21e64088a929a
kenti <016mki69@gmail.com>
2024-07-03 02:19:34 -0300

    Metodos para calcular disponibilidad en Schedule y tests

--- src/main/java/com/theWalkingDogsApp/demo/model/schedule/Schedule.java
+++ src/main/java/com/theWalkingDogsApp/demo/model/schedule/Schedule.java
@@ -3,12 +3,13 @@ package com.theWalkingDogsApp.demo.model.schedule;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Builder
 public class Schedule {
   private List<DailyAvailability> dailyAvailabilities;
   private List<LocalDate> unavailableDates;
@@ -25,19 +26,12 @@ public class Schedule {
     dailyAvailabilities.add(dailyAvailability);
   }
 
-  public boolean isAvailable(LocalDate date, List<TimeSlot> timeSlots) {
+  public boolean isAvailable(LocalDate date, TimeSlot timeSlot) {
     if(unavailableDates.contains(date)) {
       return false;
     }
     WeekDay weekDay = WeekDay.valueOf(date.getDayOfWeek().toString());
-    if(dailyAvailabilities.stream) {
-
-    }
-
-
-    return !unavailableDates.contains(date) &&
-        date.getDayOfWeek().
-        dailyAvailabilities.stream().anyMatch(d->d.isAvailable(timeSlots));
+    return this.dailyAvailabilities.stream().filter(d -> d.getWeekDay() == weekDay).anyMatch(d -> d.getTimeSlots().contains(timeSlot));
   }
 
 }
--- src/test/java/com/theWalkingDogsApp/demo/ScheduleTest.java
+++ src/test/java/com/theWalkingDogsApp/demo/ScheduleTest.java
@@ -7,24 +7,55 @@ import com.theWalkingDogsApp.demo.model.schedule.Schedule;
 import com.theWalkingDogsApp.demo.model.schedule.TimeSlot;
 import java.time.LocalDate;
 import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ScheduleTest {
+  static Schedule schedule;
+
+
+  @BeforeAll
+  static void setUpBeforeClass() throws Exception {
+
+    List<TimeSlot> availableTimeSlots = List.of(MORNING,AFTERNOON);
+    LocalDate unavailableDate = LocalDate.of(2024,7,2);
+
+    //Disponibilidad: Todos los lunes y martes, mañana y tarde. Excepto el 2024-07-02 (Martes)
+    schedule = Schedule.builder()
+        .unavailableDates(List.of(unavailableDate))
+        .dailyAvailabilities(List.of(
+            new DailyAvailability(MONDAY, availableTimeSlots),
+            new DailyAvailability(TUESDAY,availableTimeSlots)
+        ))
+        .build();
+  }
 
   @Test
-  public void checkAvailability(){
-    List<TimeSlot> availableTimeSlots = List.of(MORNING,AFTERNOON);
-    Schedule schedule = new Schedule();
-    schedule.addUnavailableDate(LocalDate.of(2024,6,30));
-    schedule.setDailyAvailabilities(List.of(
-        new DailyAvailability(MONDAY, availableTimeSlots),
-        new DailyAvailability(TUESDAY,availableTimeSlots)
-    ));
-
-    assertTrue(schedule.isAvailable());
-
-
+  public void testUnavailableDate(){
+    LocalDate date = LocalDate.of(2024,7,2);
+    assertFalse(schedule.isAvailable(date,MORNING));
   }
+
+  @Test
+  public void testAvailableDate(){
+    LocalDate date = LocalDate.of(2024,7,1);
+    assertTrue(schedule.isAvailable(date,MORNING));
+  }
+
+  @Test
+  public void testUnavailableTimeSlot(){
+    LocalDate date = LocalDate.of(2024,7,16); //lunes
+    assertFalse(schedule.isAvailable(date,EVENING));
+  }
+
+  @Test
+  public void testUnavailableWeekDay(){
+    LocalDate date = LocalDate.of(2024,7,3);
+    assertFalse(schedule.isAvailable(date,MORNING));
+  }
+
 }
