5b112ff02ead2b865cf881340fb5c4efa1d04a60
kenti <016mki69@gmail.com>
2024-11-28 00:29:24 -0300

    fasfas

--- src/main/java/com/theWalkingDogsApp/demo/model/user/User.java
+++ src/main/java/com/theWalkingDogsApp/demo/model/user/User.java
@@ -24,7 +24,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Builder
 @AllArgsConstructor
 @Data
-@Table(name="\"User\"")
+@Table(name = "users")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
