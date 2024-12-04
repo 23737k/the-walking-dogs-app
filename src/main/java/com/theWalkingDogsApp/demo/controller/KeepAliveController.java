df87d05ccc9bea8579a27f83e88967d9375f200e
kenti <016mki69@gmail.com>
2024-12-04 20:55:11 -0300

    Hide keepAlive endpoint from swagger

--- src/main/java/com/theWalkingDogsApp/demo/controller/KeepAliveController.java
+++ src/main/java/com/theWalkingDogsApp/demo/controller/KeepAliveController.java
@@ -1,10 +1,12 @@
 package com.theWalkingDogsApp.demo.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RestController
 @RequestMapping("${backend.api.base-path}/keepAlive")
 public class KeepAliveController {
